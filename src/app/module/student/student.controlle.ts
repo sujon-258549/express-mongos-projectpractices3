b187ea974c6896fb0ac5aa79b55b82241d9dcb40
Md Sujon Mia <sujon258549@gmail.com>
2024-12-01 19:31:57 +0600

    global success create

--- src/app/module/student/student.controlle.ts
+++ src/app/module/student/student.controlle.ts
@@ -3,46 +3,40 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { studentServeses } from './student.servises';
 import catchAsynch from '../utility/catcingAsynch';
+import sendSuccess from '../utility/send-success';
 
 //hairorder function
 
-const findStudent = catchAsynch(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async (req: Request, res: Response, next: NextFunction) => {
-    const result = await studentServeses.findAllStudentData();
-    res.status(200).json({
-      success: true,
-      message: 'Students retrieved successfully',
-      result,
-    });
-  },
-);
+const findStudent = catchAsynch(async (req: Request, res: Response) => {
+  const result = await studentServeses.findAllStudentData();
+  sendSuccess(res, {
+    statuscod: httpStatus.OK,
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
 
-const studentOneDeleted = catchAsynch(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { studentId } = req.params;
-
-    // Call the service to delete the student
-    const result = await studentServeses.deletedStudentone(studentId);
-    res.status(200).json({
-      success: true,
-      message: 'Student deleted successfully',
-      result,
-    });
-  },
-);
-const studentOnefind = catchAsynch(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { studentId } = req.params;
-    // Call the service to delete the student
-    const result = await studentServeses.findOnedStudent(studentId);
-    res.status(200).json({
-      success: true,
-      message: 'Student find one successfully',
-      result,
-    });
-  },
-);
+const studentOneDeleted = catchAsynch(async (req: Request, res: Response) => {
+  const { studentId } = req.params;
+  // Call the service to delete the student
+  const result = await studentServeses.deletedStudentone(studentId);
+  res.status(200).json({
+    success: true,
+    message: 'Student deleted successfully',
+    result,
+  });
+});
+const studentOnefind = catchAsynch(async (req: Request, res: Response) => {
+  const { studentId } = req.params;
+  // Call the service to delete the student
+  const result = await studentServeses.findOnedStudent(studentId);
+  res.status(200).json({
+    success: true,
+    message: 'Student find one successfully',
+    result,
+  });
+});
 export const studentController = {
   findStudent,
   studentOneDeleted,
