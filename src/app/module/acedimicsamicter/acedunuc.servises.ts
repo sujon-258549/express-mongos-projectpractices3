26fd8dc5a28b097ec9a90ac2f506a5f3cda59e3f
Md Sujon Mia <sujon258549@gmail.com>
2024-12-04 18:31:33 +0600

    // validaciton samester and cod  no match error show flow down step

--- src/app/module/acedimicsamicter/acedunuc.servises.ts
+++ src/app/module/acedimicsamicter/acedunuc.servises.ts
@@ -2,6 +2,28 @@ import { AcademicModel } from './acedimic.mode';
 import { TAcademicsamester } from './interfaces';
 
 const createAcedimic = async (payloade: TAcademicsamester) => {
+  // validaciton samester and cod  no match error show flow down step
+  type TacadimicSamsesterMaper = {
+    [kye: string]: string;
+  };
+
+  const acadimicSamestermaper: TacadimicSamsesterMaper = {
+    Autom: '01',
+    Summer: '02',
+    Fall: '03',
+  };
+
+  //   top solition 1one line cod
+  // const acadimicSamestermaper: Record<string, string> = {
+  //     Autom: '01',
+  //     Summer: '02',
+  //     Fall: '03',
+  //   };
+
+  if (acadimicSamestermaper[payloade.name] !== payloade.code) {
+    throw new Error('The semester name and code do not match.');
+  }
+
   const result = await AcademicModel.create(payloade);
   return result;
 };
