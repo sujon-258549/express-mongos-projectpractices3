66fc29f63b227423ac9486eb98f719d5955a8bdf
Md Sujon Mia <sujon258549@gmail.com>
2024-12-05 20:41:07 +0600

    modified acadimic.Faculty.model.ts and acadimic.faculty.validaction.zod.ts in acadimicFaculty

--- src/app/module/acadimicFaculty/acadimic.Faculty.model.ts
+++ src/app/module/acadimicFaculty/acadimic.Faculty.model.ts
@@ -1,8 +1,17 @@
 import { model, Schema } from 'mongoose';
 import { TFaculty } from './acadimic.Faculty.interfaces';
 
-export const acadimicSchema = new Schema<TFaculty>({
-  name: String,
-});
+export const acadimicSchema = new Schema<TFaculty>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required.'],
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export const AcadimicFucaltyModel = model<TFaculty>('Fucalty', acadimicSchema);
--- src/app/module/acadimicFaculty/acadimic.faculty.validaction.zod.ts
+++ src/app/module/acadimicFaculty/acadimic.faculty.validaction.zod.ts
@@ -1,9 +1,7 @@
-import { required } from 'joi';
 import { z } from 'zod';
 
 const fucaltyZodValidactionSchema = z.object({
   name: z.string({ invalid_type_error: 'Acadimic fuculty must be String' }),
-  required: true,
 });
 
 export const userValidalidaction = {
