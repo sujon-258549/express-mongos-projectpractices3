fd83baba71016e534dc9d5b0e4801ddcc004f720
Md Sujon Mia <sujon258549@gmail.com>
2024-12-05 19:20:19 +0600

    fucaltyZodValidactionSchema

--- src/app/module/acadimicFaculty/acadimic.fucalty.servises.ts
+++ src/app/module/acadimicFaculty/acadimic.fucalty.servises.ts
@@ -1,15 +1,15 @@
-// import { TFaculty } from './acadimic.Faculty.interfaces';
-// import { AcadimicFucaltyModel } from './acadimic.Faculty.model';
+import { TFaculty } from './acadimic.Faculty.interfaces';
+import { AcadimicFucaltyModel } from './acadimic.Faculty.model';
 
-// const createFucalty = async (payload: TFaculty) => {
-//   try {
-//     const result = await AcadimicFucaltyModel.create(payload);
-//     return result;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+const createFucalty = async (payload: TFaculty) => {
+  try {
+    const result = await AcadimicFucaltyModel.create(payload);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// export const fucaltyServises = {
-//   createFucalty,
-// };
+export const fucaltyServises = {
+  createFucalty,
+};
