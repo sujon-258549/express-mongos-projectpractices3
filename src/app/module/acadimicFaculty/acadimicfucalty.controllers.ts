055a9fd0e441442a32bfab8e96787796540c806d
Md Sujon Mia <sujon258549@gmail.com>
2024-12-05 19:52:46 +0600

    modified acadimicfucalty.controllers.ts in acadimicFaculty

--- src/app/module/acadimicFaculty/acadimicfucalty.controllers.ts
+++ src/app/module/acadimicFaculty/acadimicfucalty.controllers.ts
@@ -1,17 +1,17 @@
-// import catchAsynch from '../utility/catcingAsynch';
-// import sendSuccess from '../utility/send-success';
-// import { fucaltyServises } from './acadimic.fucalty.servises';
+import catchAsynch from '../utility/catcingAsynch';
+import sendSuccess from '../utility/send-success';
+import { fucaltyServises } from './acadimic.fucalty.servises';
 
-// const createFucalty = catchAsynch(async (req, res) => {
-//   const result = await fucaltyServises.createFucalty(req.body);
-//   sendSuccess(res, {
-//     statuscod: httpStatus.CREATED,
-//     success: true,
-//     message: 'Fucalty Created Successfuly',
-//     data: result,
-//   });
-// });
+const createFucalty = catchAsynch(async (req, res) => {
+  const result = await fucaltyServises.createFucalty(req.body);
+  sendSuccess(res, {
+    statuscod: httpStatus.CREATED,
+    success: true,
+    message: 'Fucalty Created Successfuly',
+    data: result,
+  });
+});
 
-// export const fucultyContruller = {
-//   createFucalty,
-// };
+export const fucultyContruller = {
+  createFucalty,
+};
