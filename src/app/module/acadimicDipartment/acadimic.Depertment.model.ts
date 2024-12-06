dfd6658be58e0fe6d5351669ed9dd3da2437f8c0
Md Sujon Mia <sujon258549@gmail.com>
2024-12-06 20:28:22 +0600

    acadimicDepertmentSchema

---
+++ src/app/module/acadimicDipartment/acadimic.Depertment.model.ts
@@ -0,0 +1,13 @@
+import { Schema } from 'mongoose';
+import { TAcadimicDepertment } from './acadimic.depert ment.interface';
+
+export const acadimicDepertmentSchema = new Schema<TAcadimicDepertment>({
+  name: {
+    type: String,
+    required: [true, 'name is Requerd'],
+  },
+  id: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'name is Requerd'],
+  },
+});
