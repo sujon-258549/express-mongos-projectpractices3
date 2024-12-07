666276ab054a367b7297b5c9831a272ad4439a7e
Md Sujon Mia <sujon258549@gmail.com>
2024-12-07 22:52:29 +0600

    modified globalErrorhandaler.ts in madelware

--- src/app/madelware/globalErrorhandaler.ts
+++ src/app/madelware/globalErrorhandaler.ts
@@ -1,3 +1,4 @@
+import { path } from 'path';
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -8,9 +9,21 @@ const globalErrorhandlata: ErrorRequestHandler = (error, req, res, next) => {
   const statusCode = 500;
   const message = error.message || 'Something went wrong.';
 
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  };
+
+  const errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'something went wrond',
+    },
+  ];
   res.status(statusCode).json({
     success: false,
     message: message,
+    errorSource,
     error: error,
   });
 };
