ef3bf3484f2ab3b969fccae6512ea4c047a75dc5
Md Sujon Mia <sujon258549@gmail.com>
2024-12-16 22:02:03 +0600

    modified smesterRagistaction.servises.ts in samesterRagistactoin

--- src/app/module/samesterRagistactoin/smesterRagistaction.servises.ts
+++ src/app/module/samesterRagistactoin/smesterRagistaction.servises.ts
@@ -7,9 +7,19 @@ import { SemesterRegistrationModel } from './smesterRagistaction.model';
 const createRagistaction = async (payload: TSemesterRegistration) => {
   const acadimicSamester = payload.academicSemester;
 
+  const isThereUpcamingorongoing = await SemesterRegistrationModel.findOne({
+    $or: [{ status: 'UPCOMING' }, { status: 'ONGOING' }],
+  });
+  if (isThereUpcamingorongoing) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `There is Alrady a ${isThereUpcamingorongoing?.status}`,
+    );
+  }
   const isSamesterRagistaction = await SemesterRegistrationModel.findOne({
     acadimicSamester,
   });
+
   if (isSamesterRagistaction) {
     throw new AppError(httpStatus.CONFLICT, 'Acadimic Samester Alrady exis!');
   }
@@ -28,20 +38,16 @@ const findAllRagistaction = async (query: Record<string, unknown>) => {
   const semisterQueryAlldata = new QueryBuilder(
     SemesterRegistrationModel.find().populate('academicSemester'),
     query,
-  );
-  // .filter()
-  // .sort()
-  // .paginate()
-  // .fields();
+  )
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
 
   const result = await semisterQueryAlldata.modelQuery;
   return result;
 };
 
-// const findAllRagistaction = async (query: Record<string, unknown>) => {
-//   const result = await SemesterRegistrationModel.find();
-//   return result;
-// };
 const findoneRagistaction = async (id: string) => {
   const result = await SemesterRegistrationModel.findById(id);
   return result;
