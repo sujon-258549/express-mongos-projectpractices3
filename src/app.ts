dd1b98e5b56e691f073b8ccd8b25cb7d65f759d3
Md Sujon Mia <sujon258549@gmail.com>
2025-01-14 23:59:23 +0600

    modified app.ts in src

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import cookieParser from 'cookie-parser';
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5001' }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use('/api/v1', router);
 
